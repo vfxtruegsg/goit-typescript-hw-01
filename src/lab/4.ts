7f837782a76fd1556d55ae092ac257a74f4d17ad
Andrew <andrejpopkov565@gmail.com>
2025-04-04 13:49:50 +0300

    5lb

--- src/lab/4.ts
+++ src/lab/4.ts
@@ -1,11 +1,58 @@
-function showMessage(message: string): void {
-  console.log(message);
+// Задача 3.2.9
+function maxActivities(activities: [number, number][]): number[] {
+  activities.sort((a, b) => a[1] - b[1]);
+
+  let result: number[] = [];
+  let lastEndTime = -1;
+
+  for (let i = 0; i < activities.length; i++) {
+    if (activities[i][0] > lastEndTime) {
+      result.push(i + 1);
+      lastEndTime = activities[i][1];
+    }
+  }
+  return result;
 }
 
-function calc(num1: number, num2: number): number {
-  return num1 + num2;
+const activities: [number, number][] = [
+  [1, 2],
+  [3, 4],
+  [0, 6],
+  [5, 7],
+  [8, 9],
+  [5, 9],
+];
+console.log("Максимальное количество действий:", maxActivities(activities));
+
+// Задача 3.2.10
+interface Task {
+  deadline: number;
+  profit: number;
 }
 
-function customError(): never {
-  throw new Error("Error");
+function maxProfitTasks(tasks: Task[], intervals: number): number {
+  tasks.sort((a, b) => b.profit - a.profit);
+
+  let result: (Task | null)[] = new Array(intervals).fill(null);
+  let totalProfit = 0;
+
+  for (let task of tasks) {
+    for (let j = Math.min(intervals, task.deadline) - 1; j >= 0; j--) {
+      if (!result[j]) {
+        result[j] = task;
+        totalProfit += task.profit;
+        break;
+      }
+    }
+  }
+  return totalProfit;
 }
+
+const tasks: Task[] = [
+  { deadline: 2, profit: 100 },
+  { deadline: 1, profit: 50 },
+  { deadline: 2, profit: 10 },
+  { deadline: 1, profit: 20 },
+];
+
+console.log("Максимальная прибыль:", maxProfitTasks(tasks, 2));
--- src/lab/5.ts
+++ src/lab/5.ts
@@ -1,13 +1,1 @@
-enum DayOfWeek {
-  Monday,
-  Tuesday,
-  Wednesday,
-  Thursday,
-  Friday,
-  Saturday,
-  Sunday,
-}
 
-const isWeekend = (day: DayOfWeek): boolean => {
-  return DayOfWeek.Saturday === day || DayOfWeek.Sunday === day;
-};
